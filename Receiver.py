d7d5c4a9f43a252eb50d5a42198d52063694d141
chrishsu <chia-hao.hsu@aiesec.net>
2015-10-31 22:15:01 -0400

    Finish first version of Sender and Receiver, can communicate with packets now

--- Receiver.py
+++ Receiver.py
@@ -12,30 +12,37 @@ def argv_reader(argv):
     else:
        return argv[1], int(argv[2])
 
-localhost    = socket.gethostbyname(socket.gethostname())
+localhost    = "localhost"#socket.gethostbyname(socket.gethostname())
 default_port = 8080
 
 class Receiver(object):
-      def __init__(self, addr, port):
-          self.recv_sock = init_recv_socket((addr, port))
+      def __init__(self, recv_ip, recv_port, send_ip, send_port):
+          self.recv_sock = init_recv_socket((recv_ip, recv_port))
           self.connections = [self.recv_sock]
-          self.recv_addr = addr
-          self.recv_port = port
+          self.recv_ip   = recv_ip
+          self.recv_port = recv_port
+          self.send_addr = (send_ip, int(send_port))
+
+      def send_open_request(self):
+          print "send open request"
+          self.recv_sock.sendto("I need a sender~", self.send_addr)
 
       def receiver_loop(self):
           status = True;
-          print "start Pyle Transfer Reciever on %s with port %s ..." % (self.recv_addr, self.recv_port)
+          print "start Pyle Transfer Reciever on %s with port %s ..."          \
+                                        % (self.recv_ip, self.recv_port)
           while status:
                 try:
-                   read_sockets, write_sockets, error_sockets =            \
+                    read_sockets, write_sockets, error_sockets =               \
                                  select.select(self.connections, [], [], 1)
-
-                   for socket in read_sockets:
-                       if socket is self.recv_sock: # msg from server
-                          print "it is receiver self socket."
-                       else: # msg from user to type in
-                          print "it is sender socket, yeah!"
-                          status = False
+                    if not read_sockets:
+                        continue
+                    else:
+                        send_content, send_addr = self.recv_sock.recvfrom(RECV_BUFFER)
+                        print send_content
+                        print "receive socket, song!"
+                        print "send something back"
+                        self.recv_sock.sendto("Hey you sender!", send_addr)
 
                 except KeyboardInterrupt, SystemExit:
                        print "\nLeaving Pyle Transfer..."
@@ -47,6 +54,6 @@ class Receiver(object):
 
 if __name__ == "__main__":
    # addr, port = argv_reader(sys.argv)
-   addr, port = localhost, default_port
-   receiver   = Receiver(addr, port)
+   ip, port, send_ip, send_port = localhost, default_port, localhost, default_port + 1
+   receiver = Receiver(ip, port, send_ip, send_port)
    receiver.run()
--- Sender.py
+++ Sender.py
@@ -2,7 +2,9 @@ import select
 import socket
 import sys
 
-from Utils import init_send_packet
+from Utils import RECV_BUFFER
+from Utils import init_send_socket
+
 
 def argv_reader(argv):
     if len(argv) < 3:
@@ -11,34 +13,44 @@ def argv_reader(argv):
     else:
        return argv[1], int(argv[2])
 
+localhost    = "localhost"#socket.gethostbyname(socket.gethostname())
+default_port = 8080
 
 class Sender(object):
-      def __init__(self, host, port):
-          self.sender_sock = socket(AF_INET, SOCK_DGRAM)
-          self.socket_list = [sys.stdin, self.sender_sock]
+      def __init__(self, ip, port, recv_ip, recv_port):
+          self.sender_sock = init_send_socket()
+          self.socket_list = [self.sender_sock]
+          self.recv_addr   = (recv_ip, recv_port)
+
+      def send_open_response(self):
+          self.sender_sock.sendto("lalalala", self.recv_addr)
 
       def sender_loop(self):
           status = True;
-
           while status:
                 try:
-                   read_sockets, write_sockets, error_sockets =            \
-                                 select.select(self.socket_list, [], [], 1)
-
-                   for socket in read_sockets:
-                       if socket is self.server_connect: # msg from server
-                          msg = socket.recv(RECV_BUFFER)
-
+                   recv_msg, recv_addr = self.sender_sock.recvfrom(RECV_BUFFER)
+                   print recv_msg
+                   status = False
+                   # if not read_sockets:
+                   #    print "dont receive data yet"
+                   #    self.sender_sock.sendto("lalalala", self.recv_addr)
+                   # else:
+                   #    print "receive receiver data"
+                   #    packet_bytes, self.recv_addr = socket.recvfrom(RECV_BUFFER)
+                   #    print "Receiver Message:", packet_bytes
+                   #    status = False
 
                 except KeyboardInterrupt, SystemExit:
                        print "\nLeaving Pyle Transfer..."
                        status = False
-          self.server_connect.close()
+          self.sender_sock.close()
 
       def run(self):
+          self.send_open_response
           self.sender_loop();
 
 if __name__ == "__main__":
-   host, port = argv_reader(sys.argv)
-   client     = Client(host, port)
-   client.run()
+   ip, port, recv_ip, recv_port = localhost, default_port + 1, localhost, default_port
+   sender  = Sender(ip, port, recv_ip, recv_port)
+   sender.run()
